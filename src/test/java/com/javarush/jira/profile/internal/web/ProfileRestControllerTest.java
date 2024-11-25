2439042502c2c4c0a4880f076ed89c8cec18267f
Olena Borodych <olenaborodych@gmail.com>
2024-11-25 23:40:08 +0200

    Tests are created for ProfileRestController public methods

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,8 +1,102 @@
 package com.javarush.jira.profile.internal.web;
 
 import com.javarush.jira.AbstractControllerTest;
+import com.javarush.jira.profile.ProfileTo;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.javarush.jira.common.internal.config.SecurityConfig.PASSWORD_ENCODER;
+import static com.javarush.jira.common.util.JsonUtil.writeValue;
+import static com.javarush.jira.login.internal.web.UserTestData.*;
+import static com.javarush.jira.profile.internal.web.ProfileTestData.*;
+import static com.javarush.jira.profile.internal.web.ProfileRestController.REST_URL;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 
 class ProfileRestControllerTest extends AbstractControllerTest {
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_TO_MATCHER.contentJson(USER_PROFILE_TO)); // Use PROFILE_TO_MATCHER
+    }
 
+    @Test
+    @WithUserDetails(value = GUEST_MAIL)
+    void getEmptyProfile() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_TO_MATCHER.contentJson(GUEST_PROFILE_EMPTY_TO)); // Use PROFILE_TO_MATCHER
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void update() throws Exception {
+        ProfileTo updatedTo = getUpdatedTo();
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(updatedTo)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        // Validate by fetching updated profile and matching the expected data
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_TO_MATCHER.contentJson(updatedTo)); // Use PROFILE_TO_MATCHER
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateInvalid() throws Exception {
+        ProfileTo invalidTo = getInvalidTo();
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(invalidTo)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateWithUnknownNotification() throws Exception {
+        ProfileTo unknownNotificationTo = getWithUnknownNotificationTo();
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(unknownNotificationTo)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateWithUnknownContact() throws Exception {
+        ProfileTo unknownContactTo = getWithUnknownContactTo();
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(unknownContactTo)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateHtmlUnsafeContact() throws Exception {
+        ProfileTo htmlUnsafeTo = getWithContactHtmlUnsafeTo();
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(htmlUnsafeTo)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
 }
